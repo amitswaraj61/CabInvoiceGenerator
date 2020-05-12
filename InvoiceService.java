45c19ee1caa71cec0f8001b7bb077fa0be4aa398
amitswaraj61 <aswaraj78@gmail.com>
2020-05-12 17:29:04 +0530

    Added Code Give userId and Rides Should Return Invoice Summary

---
+++ InvoiceService.java
@@ -0,0 +1,34 @@
+package cabInvoice;
+
+import java.util.ArrayList;
+
+public class InvoiceService {
+    private static final double MINIMUM_COST_PER_KILOMETER = 10;
+    private static final int COST_PER_TIME = 1;
+    private static final double MINIMUM_FARE = 5;
+    private final RideRepository rideRepository;
+
+    public InvoiceService(){
+        this.rideRepository=new RideRepository();
+    }
+
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        return Math.max(totalFare,MINIMUM_FARE);
+    }
+
+    public InvoiceSummary calculateFare(Ride[] rides){
+        double totalFare = 0;
+        for (Ride ride : rides) {
+            totalFare += this.calculateFare(ride.distance,ride.time);
+        }
+        return new InvoiceSummary(rides.length,totalFare);
+    }
+    public void addRides(String userId,Ride[] rides){
+        rideRepository.addRides(userId,rides);
+    }
+
+    public InvoiceSummary getInvoiceSummary(String userId) {
+        return this.calculateFare(rideRepository.getRides(userId));
+    }
+}
---
+++ InvoiceServiceTest.java
@@ -0,0 +1,57 @@
+package cabInvoice;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InvoiceServiceTest {
+    InvoiceService invoiceService=null;
+
+    @Before
+    public void setUp() throws Exception{
+        invoiceService = new InvoiceService();
+    }
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        InvoiceService invoiceService = new InvoiceService();
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceService.calculateFare(distance, time);
+        assertEquals(25, fare, 0.0);
+    }
+
+    @Test
+    public void givenLessDistanceOrTime_ShouldReturnMinFare() {
+        InvoiceService invoiceService = new InvoiceService();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceService.calculateFare(distance, time);
+        assertEquals(5, fare, 0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+        InvoiceService invoiceService = new InvoiceService();
+        Ride[] rides = {
+                new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        InvoiceSummary summary = invoiceService.calculateFare(rides);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30);
+        assertEquals(expectedInvoiceSummary, summary);
+    }
+    @Test
+    public void givenUserIdAndRides_ShouldReturnInvoiceSummary(){
+        String userId="a@b.com";
+        Ride[] rides = {
+                new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        invoiceService.addRides(userId,rides);
+        InvoiceSummary summary = invoiceService.getInvoiceSummary(userId);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2,30);
+        assertEquals(expectedInvoiceSummary,summary);
+    }
+}
